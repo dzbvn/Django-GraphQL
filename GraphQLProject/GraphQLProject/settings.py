5f28c4b55a62986a2029b991bb47e022978d3883
dzbvn <szymon.klempert@gmail.com>
2022-07-05 16:49:32 +0200

    Ignoring secret_key from django settings.py

--- GraphQLProject/GraphQLProject/settings.py
+++ GraphQLProject/GraphQLProject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mxd#*-dutjdhw5_4-39*0nfz00#5cuntlr*uuqhktvqjurdt9_'
+with open('./GraphQLProject/secretkey.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
