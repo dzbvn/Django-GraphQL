33abfe75c59a4e2ba7dc1321ea7bff7185f31acb
dzbvn <szymon.klempert@gmail.com>
2022-07-07 10:23:02 +0200

    Init commit of Tutorial Part 2

--- GraphQLProject/GraphQLProject/schema.py
+++ GraphQLProject/GraphQLProject/schema.py
@@ -1,9 +1,20 @@
 import graphene
 import events.schema
+import users.schema
+
 
 # Query for getting the data from the server.
-class Query(events.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, events.schema.Query, graphene.ObjectType):
     pass
 
+# Mutation for sending the data to the server.
+class Mutation(users.schema.Mutation, events.schema.Mutation, graphene.ObjectType):
+    pass
+    '''
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+    '''
+
 # Create schema
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
